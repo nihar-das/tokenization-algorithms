0f6bdde2b36b4059eaf4441595ac2e37a7d27f30
nihardas <cipherxo1@protonmail.com>
2026-04-27 21:00:09 +0530

    BPE-3: done with max-heap connection and testing

--- byte-pair-encoding/byte_pair_encoding.py
+++ byte-pair-encoding/byte_pair_encoding.py
@@ -1,19 +1,23 @@
 from collections import Counter
-from dll import DoublyLinkedList
+from max_heap import MaxHeapNode, MaxHeap
 
 
 class BytePairEncoding:
     def __init__(self, text):
         self.text = text
         self.word_freq = None
-        self.word_dll = None
+        self.word_chars = None
         self.vocabs = None
         self.char_pairs = None
+        self.all_pair_scores = None
+        self.score_heap = None
 
         self.text2WordFreq()
-        self.word2dll()
+        self.word2chars()
         self.wordFreq2Vocab()
         self.compute_char_pair()
+        self.score_all_pair()
+        self.build_score_heap()
 
     def text2WordFreq(self):
         if self.word_freq is None:
@@ -26,15 +30,11 @@ class BytePairEncoding:
                     word_freq[act_word] += 1
             self.word_freq = word_freq
 
-    def word2dll(self):
-        if self.word_dll is None:
-            word_dll = {}
-            for word in self.word_freq.keys():
-                dll = DoublyLinkedList()
-                for char in word:
-                    dll.insert(char)
-                word_dll[word] = dll
-            self.word_dll = word_dll
+    def word2chars(self):
+        word_chars = {}
+        for word in self.word_freq.keys():
+            word_chars[word] = [char for char in word]
+        self.word_chars = word_chars
 
     def wordFreq2Vocab(self):
         if self.vocabs is None:
@@ -55,10 +55,24 @@ class BytePairEncoding:
                         char_pairs.append(pair)
             self.char_pairs = char_pairs
 
-    def score_pair(self, target_pair):
-        score = 0
-        for word in self.word_freq.keys():
-            word_pairs = list(zip(word, word[1:]))
-            pair_counter = Counter(word_pairs)
-            score += pair_counter[target_pair] * self.word_freq[word]
-        return score
+    def score_all_pair(self):
+        all_pair_scores = {}
+        for target_pair in self.char_pairs:
+            all_pair_scores[target_pair] = 0
+            for word in self.word_freq.keys():
+                word_pairs = list(zip(word, word[1:]))
+                pair_counter = Counter(word_pairs)
+                all_pair_scores[target_pair] += (
+                    pair_counter[target_pair] * self.word_freq[word]
+                )
+        self.all_pair_scores = all_pair_scores
+
+    def build_score_heap(self):
+        if self.all_pair_scores and (self.score_heap is None):
+            nodes = [
+                MaxHeapNode(self.all_pair_scores[pair], pair)
+                for pair in self.all_pair_scores.keys()
+            ]
+            heap = MaxHeap(nodes)
+            heap.build_heap()
+            self.score_heap = heap
