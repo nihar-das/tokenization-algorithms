8c0d37e2440d7414dfddb25a1c907d5a2a8ebc1a
nihardas <cipherxo1@protonmail.com>
2026-04-28 15:44:22 +0530

    BPE-05:added support to insert, and get  pair2index | moved comparison logic to node itself

--- byte-pair-encoding/max_heap.py
+++ byte-pair-encoding/max_heap.py
@@ -3,6 +3,11 @@ class MaxHeapNode:
         self.score = score
         self.pair = pair
 
+    def __gt__(self, other):
+        if self.score == other.score:
+            return self.pair < other.pair
+        return self.score > other.score
+
 
 class MaxHeap:
     def __init__(self, items):
@@ -11,22 +16,21 @@ class MaxHeap:
 
     def get_largest(self, root_idx):
         largest_idx = root_idx
-        current_node = self.max_heap[root_idx]
-        max_score = current_node.score
+        max_score_node = self.max_heap[root_idx]
         left_idx = (2 * root_idx) + 1
         right_idx = left_idx + 1
 
         # check boundary condition of indices
-        if left_idx < self.item_count and self.max_heap[left_idx].score > max_score:
+        if left_idx < self.item_count and self.max_heap[left_idx] > max_score_node:
             largest_idx = (2 * root_idx) + 1
-            max_score = self.max_heap[left_idx].score
-        if right_idx < self.item_count and self.max_heap[right_idx].score > max_score:
+            max_score_node = self.max_heap[left_idx]
+        if right_idx < self.item_count and self.max_heap[right_idx] > max_score_node:
             largest_idx = 2 * (root_idx + 1)
-            max_score = self.max_heap[right_idx].score
+            max_score_node = self.max_heap[right_idx]
 
         return largest_idx
 
-    def heapify(self, root_idx):
+    def heapify_down(self, root_idx):
         largest_idx = self.get_largest(root_idx)
 
         if largest_idx == root_idx:
@@ -38,22 +42,33 @@ class MaxHeap:
             self.max_heap[root_idx],
         )
 
-        self.heapify(largest_idx)
+        self.heapify_down(largest_idx)
 
     def build_heap(self):
         # starting from last internal node
         start_idx = (self.item_count // 2) - 1
 
         while start_idx >= 0:
-            self.heapify(start_idx)
+            self.heapify_down(start_idx)
             start_idx -= 1
+
+    def insert(self, data):
+        self.max_heap.append(data)
+        self.item_count = len(self.max_heap)
 
     def extract(self):
         self.max_heap[0], self.max_heap[self.item_count - 1] = (
             self.max_heap[self.item_count - 1],
             self.max_heap[0],
         )
-        target_item = self.max_heap[self.item_count - 1]
-        self.item_count -= 1
-        self.heapify(0)
+        target_item = self.max_heap.pop(-1)
+        self.item_count = len(self.max_heap)
+        self.heapify_down(0)
         return target_item
+
+    def pair2index(self):
+        pair_index_map = {}
+        for idx, node in enumerate(self.max_heap):
+            pair_index_map[node.pair] = idx
+
+        return pair_index_map
